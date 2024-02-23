4fb78ffa54b05222dac17138b5b003f1cc8138ac
Satya Reddy <satyakrishnareddykarri2@gmail.com>
2024-02-24 03:04:10 +0530

    sm

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !="production"){
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
@@ -10,19 +10,17 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore=require("connect-mongo");
+const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
-const userRouter= require("./routes/user.js");
-
+const userRouter = require("./routes/user.js");
 
 // MongoDB Connection URL
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl =process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 // Connect to MongoDB
 async function main() {
@@ -45,41 +43,32 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const  store=MongoStore.create({
-  mongoUrl:dbUrl,
-  crypto:{
-    secret:process.env.SECRET,
+const store = new MongoStore({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
   },
-  touchAfter:24*3600,
+  touchAfter: 24 * 3600,
 });
-store.on("error",()=>{
-  console.log("error in mongo session store",err);
-})
-
-
+store.on("error", (err) => {
+  console.log("error in mongo session store", err);
+});
 
 const sessionOptions = {
-  store:store,
+  store: store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
+    // secure: true, // Uncomment in production (requires HTTPS)
   },
 };
 
-// // Routes
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
-
-
-
+// Routes
 app.use(session(sessionOptions));
 app.use(flash());
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -89,16 +78,13 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser=req.user;
+  res.locals.currUser = req.user;
   next();
 });
 
-  
-
-// Listing and Review Routes
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
-app.use("/",userRouter);
+app.use("/", userRouter);
 
 // 404 Error Handler
 app.all("*", (req, res, next) => {
